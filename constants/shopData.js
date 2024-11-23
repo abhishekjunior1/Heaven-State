d060bb3e4609189716c307148e69100ecdc8385b
abhishekjunior1 <abhishekjunior1@gmail.com>
2024-11-23 13:20:11 +0530

    initial

---
+++ constants/shopData.js
@@ -0,0 +1,38 @@
+export const rentHouses = [
+  {
+    id: 1,
+    mainImage: "/Rent-houses/House-1/home1-1.jpg",
+    name: "House in Delhi",
+    price: "INR 43,00,000",
+  },
+  {
+    id: 2,
+    mainImage: "/Rent-houses/House-2/home2-1.jpg",
+    name: "House in Mumbai",
+    price: "INR 10,00,000",
+  },
+  {
+    id: 3,
+    mainImage: "/Rent-houses/House-3/home3-1.jpg",
+    name: "Flat in Mumbai",
+    price: "INR 10,50,000",
+  },
+  {
+    id: 4,
+    mainImage: "/Rent-houses/House-4/home4-1.jpg",
+    name: "Flat in Bangalore",
+    price: "INR 13,50,000",
+  },
+  {
+    id: 5,
+    mainImage: "/Rent-houses/House-5/home5-1.jpg",
+    name: "House in Chennai",
+    price: "INR 19,50,000",
+  },
+  {
+    id: 6,
+    mainImage: "/Rent-houses/House-6/home6-1.jpg",
+    name: "House in Hyderabad",
+    price: "INR 20,50,000",
+  },
+];
---
+++ constants/showcase.js
@@ -0,0 +1,104 @@
+export const showCase = [
+  {
+    id: 1,
+    city: "Delhi",
+    shortDescription:
+      "Delhi, the vibrant capital of India, blends rich history with modern urban charm. Known for its iconic landmarks, bustling markets, and diverse culture, it's a city that offers something for everyone.",
+    description:
+      "Delhi, India's vibrant capital, is a city of contrasts where ancient history and modernity coexist. Known for its iconic landmarks like the Red Fort and India Gate, bustling markets, and diverse cuisine, Delhi offers a rich cultural tapestry. The city's dynamic energy and historical significance make it a captivating destination.",
+    coverImage: "https://media.istockphoto.com/id/1299473217/photo/the-canopy-and-the-india-gate-at-sunset-in-new-delhi-view-from-the-national-war-memorial.jpg?s=612x612&w=0&k=20&c=lDZ08vBmcCpJOajwxsWG44L2hH82dwsyCyL9aaBbf3w=",
+    moreImages: [
+      "https://media.istockphoto.com/id/894748456/photo/national-gallery-of-modern-art-new-delhi-india.jpg?s=612x612&w=0&k=20&c=JviD44YXTqQ14fyLsP47OK7r8sTYYGl_GMNO-61ZRuE=",
+      "https://media.istockphoto.com/id/1287050440/photo/lahori-gate-main-entrance-to-the-red-fort-in-new-delhi-india.jpg?s=612x612&w=0&k=20&c=DLpiJ2yOTDl3XJj8r3KpF7wXNwASN87DNeBPMelStCE=",
+      "https://media.istockphoto.com/id/517364023/photo/lotus-temple.jpg?s=612x612&w=0&k=20&c=Ap7wHtvS2nWcIyUsEDhThIln1VOcWlm50dVv7GYcBbI=",
+      "https://media.istockphoto.com/id/165204592/photo/qutub-minar-delhi-india.jpg?s=612x612&w=0&k=20&c=YvFLPmQmlgCyX7RGZA1VpKctdC6QsChINzLSMDPNI9k=",
+    ],
+    prevHref: "/projects/6",
+    nextHref: "/projects/2",
+  },
+  {
+    id: 2,
+    city: "Mumbai",
+    shortDescription:
+      "Mumbai, the bustling financial capital of India, is known for its vibrant nightlife, iconic landmarks, and diverse culture.",
+    description:
+      "Mumbai, the financial and entertainment capital of India, is a city that never sleeps. Known for its iconic Gateway of India, Marine Drive, and Bollywood film industry, Mumbai offers a unique blend of traditional and modern experiences. Its bustling markets, diverse food scene, and vibrant nightlife make it a dynamic metropolis.",
+    coverImage: "https://media.istockphoto.com/id/1390163309/photo/beautiful-gateway-of-india-near-taj-palace-hotel-on-the-mumbai-harbour-with-many-jetties-on.jpg?s=612x612&w=0&k=20&c=igVWcohJMrAiQamImAPMDxTyjjIeQJFVVGbKYllc9Gc=",
+    moreImages: [
+      "https://media.istockphoto.com/id/860528756/photo/the-bandraworli-sea-link-mumbai-india.jpg?s=612x612&w=0&k=20&c=xT9TK7oYkP6TP62lHqP0H-9mfz9cWva4OcYEnt06cjc=",
+      "https://media.istockphoto.com/id/815229336/photo/gatway-of-india.jpg?s=612x612&w=0&k=20&c=2ssn4xVyAhcDnyjgWWmaplHfkDIWhG0oFykrJ-ui9bc=",
+      "https://media.istockphoto.com/id/1201904524/photo/the-bandra-worli-sea-link-shot-at-dusk-in-mumbai-a-famous-landmark-that-connects-the-city.jpg?s=612x612&w=0&k=20&c=aElQCX_wK1WzDkyWWzx2sE7qL_dIhCWZrhQ58KBsaVQ=",
+      "https://media.istockphoto.com/id/1325342016/photo/mumbai-skyline-wadala-sewri-lalbaug.jpg?s=612x612&w=0&k=20&c=Zu7P_lttSwIU5rLJFdPmhXW4Hy-C64bAhxA7V28LvJQ=",
+    ],
+    prevHref: "/projects/1",
+    nextHref: "/projects/5",
+  },
+  {
+    id: 3,
+    city: "Chennai",
+    shortDescription:
+      "Chennai, the cultural hub of South India, is famous for its historic temples, classical music, and beautiful beaches.",
+    description:
+      "Chennai, the capital of Tamil Nadu, is a vibrant city known for its rich cultural heritage, classical music, and dance. Famous for its historic temples like Kapaleeshwarar Temple and Marina Beach, Chennai offers a blend of traditional and modern attractions. The city is also a major center for the automotive and technology industries.",
+    coverImage: "https://media.istockphoto.com/id/482020010/photo/ripon-building.jpg?s=612x612&w=0&k=20&c=3G_-A9dEfvXcaQLJvHsfPluXqdtKGTXbgwk3cFNi9z8=",
+    moreImages: [
+      "https://media.istockphoto.com/id/1430355438/photo/bangalore-cityscape-night-aerial-bengaluru-city-drone.jpg?s=612x612&w=0&k=20&c=VA3pxW2Gedikme6Y533y2D8_qy5hNEfOSMXVSXpe-tg=",
+      "https://media.istockphoto.com/id/172918982/photo/at-the-beach-of-chennai.jpg?s=612x612&w=0&k=20&c=DOQWGWKi0trnn9RGaO8MogMVRpJRAS7vUiwonx7uUKA=",
+      "https://media.istockphoto.com/id/153568198/photo/santhome-bascillica.jpg?s=612x612&w=0&k=20&c=kCl-F5Gcim4uovW_t3IjNBIQfNEbEvG6G0E5rNV7hFE=",
+      "https://media.istockphoto.com/id/1325229615/photo/sunrise-at-the-beach-with-long-walk-way-bridge-and-water-waves-are-hitting-costal-area.jpg?s=612x612&w=0&k=20&c=JLQz7QaRx7E1RztkFSgkMZDseEPQQiG7bwQtIR9uqTI=",
+    ],
+    prevHref: "/projects/2",
+    nextHref: "/projects/4",
+  },
+  {
+    id: 4,
+    city: "Bangalore",
+    shortDescription:
+      "Bangalore, the Silicon Valley of India, is renowned for its thriving tech industry, pleasant climate, and green spaces.",
+    description:
+      "Bangalore, officially known as Bengaluru, is the capital of Karnataka and the tech hub of India. Known for its pleasant climate, lush parks, and vibrant nightlife, Bangalore is home to numerous startups and multinational tech companies. The city is also famous for its historical sites like Bangalore Palace and Lalbagh Botanical Garden.",
+    coverImage: "https://media.istockphoto.com/id/1433555981/photo/vidhana-soudha-in-bangalore-india-the-seat-of-the-state-legislature-of-karnataka.jpg?s=612x612&w=0&k=20&c=cruiVCYcKeFM7axGFX1m_K95rgDMzAQlS2ZUhQGIKYc=",
+    moreImages: [
+      "https://media.istockphoto.com/id/186255464/photo/bangalore-skyline-india.jpg?s=612x612&w=0&k=20&c=_kUTev1oSISvvP60lPxNAv9QZcwKOj3djjXF3zUxFLc=",
+      "https://media.istockphoto.com/id/1430355438/photo/bangalore-cityscape-night-aerial-bengaluru-city-drone.jpg?s=612x612&w=0&k=20&c=VA3pxW2Gedikme6Y533y2D8_qy5hNEfOSMXVSXpe-tg=",
+      "https://media.istockphoto.com/id/1053591264/photo/ulsoor-lake.jpg?s=612x612&w=0&k=20&c=_YgSyM2JD5nNa0Fvln2H1cGGFiZmqwxUTrEdV17AIh8=",
+      "https://media.istockphoto.com/id/1382384327/photo/bangalore-or-bengaluru.jpg?s=612x612&w=0&k=20&c=MPlI0Z3GfvkuzL5i2tHefcDDOsYNGevkr39lGEjAoR4=",
+    ],
+    prevHref: "/projects/3",
+    nextHref: "/projects/5",
+  },
+  {
+    id: 5,
+    city: "Hyderabad",
+    shortDescription:
+      "Hyderabad, the historic city of Nizams, is famous for its rich history, stunning architecture, and delectable cuisine.",
+    description:
+      "Hyderabad, the capital of Telangana, is known for its rich history, culture, and cuisine. The city is home to iconic landmarks like Charminar, Golconda Fort, and the beautiful Hussain Sagar Lake. Hyderabad's vibrant culture is reflected in its bazaars, where one can find traditional crafts and the famous Hyderabadi biryani.",
+    coverImage: "https://media.istockphoto.com/id/1215274990/photo/high-wide-angle-view-of-charminar-in-the-night.jpg?s=612x612&w=0&k=20&c=byyIjqgbslf-L191n6SJu0s35fvNoVeWsxV5rIPK7Sk=",
+    moreImages: [
+      "/Project-showcase/Peshawar/Islamia-college.jpg",
+      "/Project-showcase/Peshawar/Sir-cunningham-clock-tower.jpg",
+      "/Project-showcase/Peshawar/Sethi-house.jpg",
+      "/Project-showcase/Peshawar/Peshawar-museum.jpg",
+    ],
+    prevHref: "/projects/4",
+    nextHref: "/projects/6",
+  },
+  {
+    id: 6,
+    city: "Shimla",
+    shortDescription:
+      "Shimla, the charming hill station in the Himalayas, attracts visitors with its cool climate, colonial architecture, and panoramic views.",
+    description:
+      "Shimla, the capital of Himachal Pradesh, is a picturesque hill station known for its colonial architecture, pleasant weather, and stunning landscapes. Popular attractions include The Ridge, Mall Road, and Jakhoo Temple. Shimla's lush green hills and serene environment make it a perfect retreat from the hustle and bustle of city life.",
+    coverImage: "/Project-showcase/Murree/Hills-view.jpg",
+    moreImages: [
+      "/Project-showcase/Murree/Attabad-lake.jpg",
+      "/Project-showcase/Murree/Chair-lifts.jpg",
+      "/Project-showcase/Murree/Jhika-gali.jpg",
+      "/Project-showcase/Murree/Murree-city.jpg",
+    ],
+    prevHref: "/projects/5",
+    nextHref: "/projects/1",
+  },
+];
